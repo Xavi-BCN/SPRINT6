beccf93816b5c3a6b1f669f3b31c3fe5fb0ae602
Xavi <xcalduch@gmail.com>
2022-11-16 17:48:43 +0100

    Start exercise 6

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ const App = () => {
   const [position, setPosition] = useState(0);
   const [init, setInit] = useState(true);
   const changeInit = () => setInit(false)
+  
   const movePrevius = () => {
     position > 0 ? setPosition(position - 1) : alert('Ja estàs al principi');
+
   };
   const moveNext = () => {
     position < 3 ? setPosition(position + 1) : alert('Ja estàs al final');
@@ -27,7 +29,7 @@ const App = () => {
           <Button text='Anterior' manejarClick={movePrevius} />
           <Button text='Següent'  manejarClick={moveNext} />
           {Phrases.map((item, i) => (
-            <Escena key={i} id={i} showText={item.text} paintPink={position}/>
+            <Escena key={i} id={i} showText={item.text} paintPink={position} imgsrc={item.img} />
           ))}
         </>
       );
@@ -36,3 +38,9 @@ const App = () => {
 }
 export default App;
 
+
+//<div style={{backgroundImage: url(`{./assets/${imgsrc}}`)}}>
+
+//<document.body style ={{ backgroundImage: url(`imgsrc`) }} ></document.body>
+// {document.body.style='backgraound-url'{position + 1}''};
+//<Document.body.style.backgroundImage: url(`./assets/{imgsrc}`)>  
--- src/components/escena/Escena.jsx
+++ src/components/escena/Escena.jsx
@@ -1,13 +1,13 @@
 import { StylNormal } from './stylEscenaNormal';
 import { StylPink } from './stylEscenaPink';
 
-export const Escena = ({ showText, id, paintPink }) => {
+export const Escena = ({ showText, id, paintPink, imgsrc }) => {
   
   const WhatStyl = paintPink === id? StylPink : StylNormal
 
   return (
     <WhatStyl>
-      <h4 id={id}>{showText}</h4>
+      <h4 id={id} path={imgsrc} >{showText}</h4>
     </WhatStyl>
   );
 }
