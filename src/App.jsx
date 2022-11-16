eb34db65412330a278e82f6347646126f94f469c
Xavi <xcalduch@gmail.com>
2022-11-16 12:54:11 +0100

    Exercise 5 OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Welcome} from './components/Welcome'
 import { Button } from './components/Button'
 import { Phrases } from './phrases'
 import { Escena } from './components/escena/Escena'
@@ -5,21 +6,33 @@ import { useState } from 'react';
 
 const App = () => {
   const [position, setPosition] = useState(0);
+  const [init, setInit] = useState(true);
+  const changeInit = () => setInit(false)
   const movePrevius = () => {
     position > 0 ? setPosition(position - 1) : alert('Ja estàs al principi');
   };
   const moveNext = () => {
     position < 3 ? setPosition(position + 1) : alert('Ja estàs al final');
-  }
-  return (
-    <>
-      <Button text='Anterior' manejarClick={movePrevius} />
-      <Button text='Següent'  manejarClick={moveNext} />
-      {Phrases.map((item, i) => (
-        <Escena key={i} id={i} showText={item.text} paintPink={position}/>
-      ))}
-    </>
-  );
+  };
+  
+  if(init){ 
+    return (
+      <>
+      <Welcome btnStart={changeInit} />
+      </>
+     )
+    }else{
+      return (
+        <>
+          <Button text='Anterior' manejarClick={movePrevius} />
+          <Button text='Següent'  manejarClick={moveNext} />
+          {Phrases.map((item, i) => (
+            <Escena key={i} id={i} showText={item.text} paintPink={position}/>
+          ))}
+        </>
+      );
+
+    }
 }
 export default App;
 
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import '../styles/Welcome.css';
+import history from '../assets/history.png';
+
+export function Welcome({ btnStart }) {
+
+  return (
+      <div className='init-form'>
+        <p>Benvinguts !!!!</p>
+        <p>Descripció del projecte</p>
+        <p>Ja has arribat al teu primer lliurament de React! Una vegada tenint els conceptes clars de javascript, veuràs que <strong>amb l'ajuda de React, podràs fer de manera fàcil i ràpida aplicacions més potents del que podíem fer amb Javascript pla</strong> (vanilla Javascript).</p>
+        <p>Pren-te amb calma aquest lliurament, els primers passos en React costen una mica, ja que has d'entendre la manera en la qual les aplicacions en React es construeixen. No oblidis que en React, la web la dividim en petits trossos o components, que són mòduls reutilitzables que representen una part de la nostra aplicació. Aquestes parts poden ser grans o petites, però, generalment, estan ben definides: tenen un propòsit únic i obvi.</p>
+        <p>En aquest lliurament posarem en pràctica els conceptes bàsics que has après de React.&nbsp;</p>
+        <p><strong>Un/a client/a</strong> que té com a producte principal una web de gestió empresarial desenvolupada amb React, ens ha demanat que <strong>desenvolupem un tutorial, en el qual mitjançant dos botons els nous usuaris puguin avançar i retrocedir en els consells, modificant-se el text d'ajuda i la imatge de fons.</strong></p>
+        <p>Com de moment no tenim el material necessari per crear el tutorial, li mostrarem al client/a els següents 4 passos:</p>
+        <img src={history} width='400px'></img>
+        <p></p>
+        <button onClick={btnStart}>Som-hi</button>
+      </div>
+  );
+}
