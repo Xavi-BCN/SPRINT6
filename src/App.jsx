7799c354ea6cd047ea46457fed9aaa26a26af7cc
Xavi <xcalduch@gmail.com>
2022-11-17 12:46:45 +0100

    Continuos Exercise 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,49 @@
-import { Welcome} from './components/Welcome'
-import { Button } from './components/Button'
-import { Phrases } from './phrases'
-import { Escena } from './components/escena/Escena'
-import { useState } from 'react';
+import { Welcome } from "./components/Welcome";
+import { Button } from "./components/Button";
+import { Phrases } from "./phrases";
+import { Escena } from "./components/escena/Escena";
+import { useState } from "react";
 
 const App = () => {
   const [position, setPosition] = useState(0);
   const [init, setInit] = useState(true);
-  const changeInit = () => setInit(false)
-  
-  const movePrevius = () => {
-    position > 0 ? setPosition(position - 1) : alert('Ja estàs al principi');
+  const changeInit = () => setInit(false);
 
+  const movePrevius = () => {
+    position > 0 ? setPosition(position - 1) : alert("Ja estàs al principi");
   };
   const moveNext = () => {
-    position < 3 ? setPosition(position + 1) : alert('Ja estàs al final');
+    position < 3 ? setPosition(position + 1) : alert("Ja estàs al final");
   };
-  
-  if(init){ 
+
+  if (init) {
     return (
       <>
-      <Welcome btnStart={changeInit} />
+        <Welcome btnStart={changeInit} />
       </>
-     )
-    }else{
-      return (
-        <>
-          <Button text='Anterior' manejarClick={movePrevius} />
-          <Button text='Següent'  manejarClick={moveNext} />
-          {Phrases.map((item, i) => (
-            <Escena key={i} id={i} showText={item.text} paintPink={position} imgsrc={item.img} />
-          ))}
-        </>
-      );
-
-    }
-}
+    );
+  } else {
+    return (
+      <>
+        <Button text="Anterior" manejarClick={movePrevius} />
+        <Button text="Següent" manejarClick={moveNext} />
+        {Phrases.map((item, i) => (
+          <Escena
+            key={i}
+            id={i}
+            showText={item.text}
+            paintPink={position}
+            imgsrc={item.img}
+          />
+        ))}
+      </>
+    );
+  }
+};
 export default App;
-
 
 //<div style={{backgroundImage: url(`{./assets/${imgsrc}}`)}}>
 
 //<document.body style ={{ backgroundImage: url(`imgsrc`) }} ></document.body>
 // {document.body.style='backgraound-url'{position + 1}''};
-//<Document.body.style.backgroundImage: url(`./assets/{imgsrc}`)>  
+//<Document.body.style.backgroundImage: url(`./assets/{imgsrc}`)>
--- src/components/escena/Escena.jsx
+++ src/components/escena/Escena.jsx
@@ -1,13 +1,14 @@
-import { StylNormal } from './stylEscenaNormal';
-import { StylPink } from './stylEscenaPink';
+import { StylNormal } from "./stylEscenaNormal";
+import { StylPink } from "./stylEscenaPink";
 
 export const Escena = ({ showText, id, paintPink, imgsrc }) => {
-  
-  const WhatStyl = paintPink === id? StylPink : StylNormal
+  const WhatStyl = paintPink === id ? StylPink : StylNormal;
 
   return (
     <WhatStyl>
-      <h4 id={id} path={imgsrc} >{showText}</h4>
+      <h4 id={id} path={imgsrc}>
+        {showText}
+      </h4>
     </WhatStyl>
   );
-}
+};
