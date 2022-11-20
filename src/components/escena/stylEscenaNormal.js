223a8761f94d2e37e08f2df69c668e5e8edd00bc
Xavi <xcalduch@gmail.com>
2022-11-20 19:47:19 +0100

    Exercise 6 OK CSS Correct

--- src/components/escena/stylEscenaNormal.js
+++ src/components/escena/stylEscenaNormal.js
@@ -6,5 +6,8 @@ export const StylNormal = StylEscenaNormal.div`
     border: 2px solid black;
     border-radius: 60px; 
     width: 800px;
+    background-color: rgb(255,255,255, 0.5);
+    color: black;
+
 
 `;
